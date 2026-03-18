7318d99eff9bcb37b8311ac008ece64292041188
Tayo <omotayo_dnl@yahoo.com>
2026-03-18 09:26:43 -0500

    added fourth parameter to middleware

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const { INTERNAL_SERVER_ERROR } = require("./utils/errors");
 const { login, createUser } = require("./controllers/users");
 const auth = require("./middlewares/auth");
+const { getItems } = require("./controllers/clothingItems");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -12,19 +14,22 @@ const mainRouter = require("./routes");
 
 mongoose.connect("mongodb://localhost:27017/wtwr_db");
 
+app.use(cors());
 app.use(express.json());
 
 app.post("/signin", login);
 app.post("/signup", createUser);
+app.get("/items", getItems);
 
 app.use(auth);
 
 app.use(mainRouter);
 
-app.use((err, req, res) =>
-  res.status(err.statusCode || INTERNAL_SERVER_ERROR).send({
+app.use((err, req, res, next) => {
+  void next;
+  return res.status(err.statusCode || INTERNAL_SERVER_ERROR).send({
     message: err.message || "An error has occurred on the server.",
-  })
-);
+  });
+});
 
 app.listen(PORT);
